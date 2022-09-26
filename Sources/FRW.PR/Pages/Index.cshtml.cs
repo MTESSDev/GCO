c2dcbe50b84a8e9434275abd2996063d5ef9f910
anisite <infologique@gmail.com>
2022-09-26 10:43:45 -0400

    Initial commit

---
+++ Sources/FRW.PR/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FRW.PR.Extra.Pages.GCO
+{
+    public class GCOIndexModel : PageModel
+    {
+        public GCOIndexModel()
+        {
+        }
+    }
+}
