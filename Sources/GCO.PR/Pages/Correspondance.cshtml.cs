93aee99a1aa3e74a46633fc85624bd1818227be4
j0ber <chriscaux@hotmail.com>
2022-10-05 12:54:52 -0400

    Correspondance

---
+++ Sources/GCO.PR/Pages/Correspondance.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GCO.PR.Pages
+{
+    public class GCOCorrespondanceModel : PageModel
+    {
+        /// <summary>
+        /// Code de correspondance
+        /// </summary>
+        public string? CodeCorrespondance { get; set; }
+  
+        /// <summary>
+        /// Code de correspondance
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public IActionResult OnGet(string c)
+        {
+            CodeCorrespondance = c;
+            return Page();
+        }
+
+    }
+}
